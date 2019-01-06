2f14d0f0ff7931156c167f82f18ac6cad5f1cd0a
Thomaszhang19 <43483538+Thomaszhang19@users.noreply.github.com>
2019-01-07 05:12:28 +0800

    Create ga.js

---
+++ ga.js
@@ -0,0 +1,63 @@
+/*! jQuery v3.3.1 | (c) JS Foundation and other contributors | jquery.org/license */ ! function(e, t) {
+    "use strict";
+    "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
+        if (!e.document) throw new Error("jQuery requires a window with a document");
+        return t(e)
+    } : t(e)
+}("undefined" != typeof window ? window : this, function(e, t) {
+    "use strict";
+    var n = [],
+        r = e.document,
+        i = Object.getPrototypeOf,
+        o = n.slice,
+        a = n.concat,
+        s = n.push,
+        u = n.indexOf,
+        l = {},
+        c = l.toString,
+        f = l.hasOwnProperty,
+        p = f.toString,
+        d = p.call(Object),
+        h = {},
+        g = function e(t) {
+            return "function" == typeof t && "number" != typeof t.nodeType
+        },
+        y = function e(t) {
+            return null != t && t === t.window
+        },
+        v = {
+            type: !0,
+            src: !0,
+            noModule: !0
+        };
+​
+    function m(e, t, n) {
+        var i, o = (t = t || r).createElement("script");
+        if (o.text = e, n)
+            for (i in v) n[i] && (o[i] = n[i]);
+        t.head.appendChild(o).parentNode.removeChild(o)
+    }
+​
+    function x(e) {
+        return null == e ? e + "" : "object" == typeof e || "function" == typeof e ? l[c.call(e)] || "object" : typeof e
+    }
+    var b = "3.3.1",
+        w = function(e, t) {
+            return new w.fn.init(e, t)
+        },
+        T = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+    w.fn = w.prototype = {
+        jquery: "3.3.1",
+        constructor: w,
+        length: 0,
+        toArray: function() {
+            return o.call(this)
+        },
+        get: function(e) {
+            return null == e ? o.call(this) : e < 0 ? this[e + this.length] : this[e]
+        },
+        pushStack: function(e) {
+ 
+
+
+
